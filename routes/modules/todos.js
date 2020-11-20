c4cce18d4c0edfdf126f4fe10555ded10dae2b5c
Ahno Chen <ahnochen1029@gmail.com>
2020-11-20 21:28:04 +0800

    add create function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,12 +4,23 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todos: todo.toJSON() }))
     .catch(error => console.log(error))
 })
-
 
 module.exports = router
