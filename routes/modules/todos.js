d78655ddb396c18cac7c0a1a57f92426f81a81db
Ahno Chen <ahnochen1029@gmail.com>
2020-11-21 00:06:21 +0800

    revise detail function to findOne

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,10 +4,11 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+//new
 router.get('/new', (req, res) => {
   return res.render('new')
 })
-
+//greate new todo
 router.post('/', (req, res) => {
   const UserId = req.user.id
   const name = req.body.name
@@ -16,11 +17,37 @@ router.post('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//get details
 router.get('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todos: todo.toJSON() }))
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
+})
+
+//edit todo page
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { UserId, id } })
+    .then(todo => res.render('edit', { todo: todo.get() }))
+    .catch(err => console.log(err))
+})
+//update todo
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  console.log(req.body)
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(err => console.log(err))
 })
 
 router.delete('/:id', (req, res) => {
