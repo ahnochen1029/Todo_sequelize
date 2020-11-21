67e61fdc92b26151c7d887f79c36ae21ebb86902
Ahno Chen <ahnochen1029@gmail.com>
2020-11-21 20:19:50 +0800

    relationship between user and todo, add flash message

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,15 +3,21 @@ const router = express.Router()
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
+  User.findByPk(req.user.id)
+    .then((user) => {
+      console.log(req.user.id)
+      if (!user) throw new Error('user not found')
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId: req.user.id }
+      })
+    })
+    .then((todos) => { return res.render('index', { todos }) })
     .catch((error) => console.log(error))
 })
-
 
 module.exports = router
