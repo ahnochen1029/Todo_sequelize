97de3691ad8afc34231333205de42ce90043bbff
Ahno Chen <ahnochen1029@gmail.com>
2020-11-21 21:33:32 +0800

    add facebook login

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 const usePassport = require('./config/passport')
 const routes = require('./routes')
 
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
